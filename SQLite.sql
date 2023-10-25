9b0d4c63b6430eaa17c27e5093c97b721b53331c
Rubinha14101505 <134503850+Rubinha14101505@users.noreply.github.com>
2023-10-25 09:28:00 -0300

    Add files via upload

---
+++ SQLite.sql
@@ -0,0 +1,15 @@
+CREATE TABLE CLIENTE(
+  ID_Cliente int NOT NULL,
+  Nome varchar(40) NOT NULL,
+  CPF char(14) NOT NULL,
+  RG char(12) NOT NULL,
+  Logradouro varchar(100) NOT NULL,
+  CEP char(9) NOT NULL,
+  UF char(2),
+  Cidade varchar(30) not NUll,
+  Telefone char(16) NOT NULL,
+  Email varchar(355) NOT NULL,
+  CNH char(9) NOT NULL,
+  Renda float (10) NOT NULL
+);
+
